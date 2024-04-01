73420d4d400223c1ddaa4e2b45c03fb14519f573
zlatanjakic <zlatan.jakic@klika.ba>
2024-04-01 18:33:23 +0200

    Set up the express webserver

--- Web/index.js
+++ Web/index.js
@@ -5,3 +5,6 @@ const path = require("path");
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.listen(3000);
+console.log("Server started");
